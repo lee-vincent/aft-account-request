d4b944869b69d7f5d546fe56a73039a5544812cc
lee-vincent <vinnie@vinnielee.io>
2023-06-12 10:57:16 -0400

    sandbox-account-01 request v2

--- sandbox-account-01-request.tf
+++ sandbox-account-01-request.tf
@@ -5,19 +5,19 @@ module "sandbox_account_01" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail = "vinnie+aft-account-1@killer.games"
+    AccountEmail = "vinnie+aft-account-01@killer.games"
     AccountName  = "sandbox-account-01"
     # Syntax for top-level OU
     ManagedOrganizationalUnit = "Sandbox"
     # Syntax for nested OU
     # ManagedOrganizationalUnit = "Sandbox (ou-xfe5-a8hb8ml8)"
-    SSOUserEmail     = "vinnie+aft-account-1@killer.games"
+    SSOUserEmail     = "vinnie+aft-account-01@killer.games"
     SSOUserFirstName = "John"
     SSOUserLastName  = "Doe"
   }
 
   account_tags = {
-    "ABC:Owner"       = "vinnie+aft-account-1@killer.games"
+    "ABC:Owner"       = "vinnie+aft-account-01@killer.games"
     "ABC:Division"    = "ENT"
     "ABC:Environment" = "Dev"
     "ABC:CostCenter"  = "123456"
@@ -27,15 +27,15 @@ module "sandbox_account_01" {
     "ABC:Project"     = "123456"
   }
 
-  # change_management_parameters = {
-  #   change_requested_by = "John Doe"
-  #   change_reason       = "testing the account vending process"
-  # }
+  change_management_parameters = {
+    change_requested_by = "John Doe"
+    change_reason       = "testing the account vending process"
+  }
 
-  # custom_fields = {
-  #   custom1 = "a"
-  #   custom2 = "b"
-  # }
+  custom_fields = {
+    custom1 = "a"
+    custom2 = "b"
+  }
 
-  # account_customizations_name = "sandbox-customizations"
+  account_customizations_name = "sandbox-customizations"
 }
